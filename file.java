6940660203d71436ef10b88732d9de3954dcbb7a
DevSecOps-SAI <devopsfdev@gmail.com>
2026-05-12 12:47:56 +0530

    Replace Flask upload script with Java program

---
+++ file.java
@@ -0,0 +1,11 @@
+package practice;
+ 
+public class Sai {
+public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	System.out.println("Welcome Everyone");
+	System.out.println("Sum:"+(a+b));
+	System.out.println("Thank you");
+}
+}
